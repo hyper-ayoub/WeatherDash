8c55821ac9dd58632dc661b8de98a6a1ba96cdba
hyper-ayoub <bouagnaayoub@gmail.com>
2026-04-23 22:18:41 +0100

    WeatherDash
    WeatherDash app

---
+++ Backend/README.md
@@ -0,0 +1,3 @@
+# WeatherDash
+ WeatherDash Django-based weather application with a focus on global world zones, 
+by hamza
---
+++ Backend/WeatherDash/Home/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import HomeViews
+
+urlpatterns = [
+    path('', HomeViews.as_view(), name ='Home'),
+]
